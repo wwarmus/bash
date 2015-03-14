d9c9cdec33e34299b412731772ed490bd23e0846
The Lorekeeper <lorekeeper@apt-forge.com>
2015-03-14 22:05:30 +0100

    Add autodetect active interface in forge-autostart

--- bin/forge-autostart.sh
+++ bin/forge-autostart.sh
@@ -4,9 +4,20 @@ set -o nounset
 # based define IPs
 readonly ALLOWED_IPS="10.11.12.30 10.11.14.30"
 
-# get current IP of the ethernet interface
-# (adjust the _INTERFACE variable)
-_INTERFACE="em1"
+# autodetect active interface by checking default route
+_INTERFACE=$(ip r s | grep default | awk '{print $5}')
+
+# since there might be no default route, autodetect may return null
+# so check is needed
+if [[ -z ${_INTERFACE} ]]; then
+  echo "No active inet device detected. Quitting"
+  exit 1
+fi
+
+# or set explicit interface get current IP of the ethernet interface
+#_INTERFACE="em1"
+
+# get current IP
 _ip=$(ifconfig ${_INTERFACE} | grep inet | awk '{print $2}')
 
 # declare commands to be run
